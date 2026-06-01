6d24dffc8c08a51a376cfba03fbbde001bea5e8f
Excalibur_0006 <sumitjangra7906.gmail.com>
2026-06-01 23:55:03 +0530

    Add LeetCode 0001: Two Sum solution

---
+++ Leetcode/001.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution 
+{
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++) 
+        {
+            int complement = target - nums[i];
+            if (mp.count(complement)) {
+                return {mp[complement], i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+int main() 
+{
+    Solution solver;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    
+    vector<int> result = solver.twoSum(nums, target);
+    
+    if (!result.empty()) {
+        cout << "Indices: " << result[0] << ", " << result[1] << endl;
+    } else {
+        cout << "No solution found." << endl;
+    }
+    
+    return 0;
+}
