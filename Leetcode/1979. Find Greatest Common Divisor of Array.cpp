2664fa64160d315edbf655f757e1029d0d977070
Excalibur677 <sumitjangra7906@gmail.com>
2026-07-18 23:59:09 +0530

    Fix LeetCode 1979: Replace C++17 structured bindings with classic loop and explicit Euclidean GCD for compiler backward compatibility

--- Leetcode/1979. Find Greatest Common Divisor of Array.cpp
+++ Leetcode/1979. Find Greatest Common Divisor of Array.cpp
@@ -1,21 +1,37 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <numeric>
 
 using namespace std;
 
 class Solution {
+private:
+    int gcd(int a, int b) {
+        while (b != 0) {
+            int t = a % b;
+            a = b;
+            b = t;
+        }
+        return a;
+    }
+
 public:
     int findGCD(vector<int>& nums) {
-        auto [mn, mx] = minmax_element(nums.begin(), nums.end());
-        return gcd(*mn, *mx);
+        int mn = nums[0];
+        int mx = nums[0];
+        
+        for (int num : nums) {
+            if (num < mn) mn = num;
+            if (num > mx) mx = num;
+        }
+        
+        return gcd(mn, mx);
     }
 };
 
 int main() {
     Solution solver;
     vector<int> nums = {2, 5, 6, 9, 10};
-    cout << solver.findGCD(nums) << endl; // Expected: 2 (gcd of 2 and 10)
+    cout << solver.findGCD(nums) << endl; // Expected: 2
     return 0;
 }
